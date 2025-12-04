283d9df68c023180d8ec60485f9571a49de02dc1
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-12-04 13:57:47 +1300

    completed card component image loading + app WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react';
+import Card from './components/card';
 import './App.css';
 
 function App() {
 
+  const playerArray = ["Jalen_Brunson", "Donovan_Mitchell", "Jayson_Tatum", "Giannis-Antetokounmpo", "Karl-Anthony_Towns", 	"Shai_Gilgeous-Alexander", "Stephen_Curry", "Lebron_James", "Kevin_Durant", "Nikola_Jokić"];
+  //randomise index to reference - will need to not duplicate
+
+  function randomPlayer(playerArray) {
+    
+  }
+
   return (
     <>
+      <Card
+        name = "Luka_Dončić"
+      />
+      <Card
+        name = "Lebron_James"
+      />
       <h1>bre</h1>
     </>
   )
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 
 async function getImage(playerName) {
@@ -9,14 +8,29 @@ async function getImage(playerName) {
         )
         const playerData = await response.json();
         console.log(playerData)
+        return playerData.player[0].strThumb
+
+    } catch (error) {
+        console.error("Fetch error:", error)
     }
 }
 
 export default function Card({ name }) {
+    const [image, setImage] = useState(null);
+
+    useEffect(() => {
+        async function loadImage() {
+            const img = await getImage(name);
+            setImage(img);
+        }
+        loadImage();
+    }, [name]); // dependency array - run this whenever name changes
+    
+    const playerName = name.replace("_", " ")
     return (
         <div className="nameCard">
-            <img src="" alt="" />
-            <h1>{name}</h1>
+            <img src={image} alt="Luka Dončić" />
+            <h1>{playerName}</h1>
         </div>
     )
 }
