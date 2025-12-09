74ec046dc2e585e9173840120c0bc38d46eed3b4
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-12-09 15:20:14 +1300

    refactored card.jsx with .map and used set
    Also now using setPlayerArray

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import CardSection from "./components/card";
 import Header from "./components/header";
 
 function App() {
 
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
   return (
     <div className='App'>
-      <Header/>
-      <CardSection/>
+      <Header gameScore={score} bestScore={bestScore}/>
+      <CardSection
+        score={score}
+        setScore={setScore}
+        bestScore={bestScore}
+        setBestScore={setBestScore}
+      />
     </div>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -15,7 +15,7 @@ async function getImage(playerName) {
     }
 }
 
-function Card({ name }) {
+function Card({ name, clickedList, setClickedList, score, setScore, bestScore, setBestScore, setPlayerArray, playerList}) {
     const [image, setImage] = useState(null);
 
     useEffect(() => {
@@ -29,54 +29,82 @@ function Card({ name }) {
     const playerName = name.replace("_", " ")
 
     return (
-        <div className="nameCard" id={name} onClick={updateClickedArray}>
+        <div
+            className="nameCard"
+            id={name}
+            onClick={(e) => updateClickedList(e, clickedList, setClickedList, score, setScore, bestScore, setBestScore, setPlayerArray, playerList)}
+        >
             <img src={image} alt={name + " image"} />
             <h1>{playerName}</h1>
         </div>
     )
 }
 
-function updateClickedArray(e, clickedArray, setClickedArray ) {
-    setClickedArray({
-        ...clickedArray,
-        [e.target.id]:e.target.id
-    })
+
+
+function updateClickedList(e, clickedList, setClickedList, score, setScore, bestScore, setBestScore, setPlayerArray, playerList) {
+
+    function clickedNameCheck(){
+        if (clickedList.has(e.currentTarget.id)) {
+            return true; 
+        }
+        return false;
+    }
+
+
+    clickedNameCheck()
+    console.log(clickedList)
+
+    if (clickedNameCheck()) {
+        setScore(0);
+        setClickedList(new Set());
+    }
+    else {
+        const updatedSet = new Set(clickedList)
+        updatedSet.add(e.currentTarget.id)
+        setClickedList(updatedSet)
+
+        setScore(score + 1)
+        if (score + 1 > bestScore) {
+            setBestScore(score +1)
+        }
+    }
+
+    setPlayerArray(shuffleArray(playerList))
+    
 }
 
-function clickedNameCheck(e, clickedArray){
-    if (clickedArray.includes(e.target.name)) {
-       return true; 
-    }
-    return false;
-}
+
 
 function shuffleArray(array) {
     return [...array].sort(() => Math.random() - 0.5);
 }
 
 
-export default function CardSection() {
+export default function CardSection({ score, setScore, bestScore, setBestScore }) {
 
     const playerList = ["Jalen_Brunson", "Donovan_Mitchell", "Jayson_Tatum", "Giannis_Antetokounmpo", "Luka_Dončić", "Kyrie_Irving", "Stephen_Curry", "Lebron_James", "Kevin_Durant", "Nikola_Jokić"];
     
-    const [playerArray] = useState(() => shuffleArray(playerList));
+    const [playerArray, setPlayerArray] = useState(shuffleArray(playerList));
 
-    const [clickedArray, setClickedArray] = useState([])
+    const [clickedList, setClickedList] = useState(new Set());
 
-    const [score, setScore] = useState(0)
-    
     return (
-        <div className="CardSection">
-            <Card name = {playerArray[0]}/>
-            <Card name = {playerArray[1]}/>
-            <Card name = {playerArray[2]}/>
-            <Card name = {playerArray[3]}/>
-            <Card name = {playerArray[4]}/>
-            <Card name = {playerArray[5]}/>
-            <Card name = {playerArray[6]}/>
-            <Card name = {playerArray[7]}/>
-            <Card name = {playerArray[8]}/>
-            <Card name = {playerArray[9]}/>
+        <div className="cardSection">
+            {playerArray.map((e) => 
+                <Card
+                    name={e}
+                    clickedList={clickedList}
+                    setClickedList={setClickedList}
+                    setPlayerArray={setPlayerArray}
+                    playerList={playerList}
+                    score={score}
+                    setScore={setScore}
+                    bestScore={bestScore}
+                    setBestScore={setBestScore}
+                />
+            )}
         </div>
     )
+
 }
